5a82b23fc116b02c09e2f6f5837cf96ac9adfbfe
Raju Sahu <naimishsahu08@gmail.com>
2021-10-14 23:01:20 +0530

    first changes

---
+++ form.php
@@ -0,0 +1,131 @@
+<?php
+  $msg = "";
+  $msg_class = "";
+  $conn = mysqli_connect("localhost", "root", "", "img-upload");
+  if (isset($_POST['save_profile'])) {
+    // for the database
+    $bio = stripslashes($_POST['bio']);
+    $profileImageName = time() . '-' . $_FILES["profileImage"]["name"];
+    // For image upload
+    $target_dir = "images/";
+    $target_file = $target_dir . basename($profileImageName);
+    // VALIDATION
+    // validate image size. Size is calculated in Bytes
+    if($_FILES['profileImage']['size'] > 200000) {
+      $msg = "Image size should not be greated than 200Kb";
+      $msg_class = "alert-danger";
+    }
+    // check if file exists
+    if(file_exists($target_file)) {
+      $msg = "File already exists";
+      $msg_class = "alert-danger";
+    }
+    // Upload image only if no errors
+    if (empty($error)) {
+      if(move_uploaded_file($_FILES["profileImage"]["tmp_name"], $target_file)) {
+        $sql = "INSERT INTO users SET profile_image='$profileImageName', bio='$bio'";
+        if(mysqli_query($conn, $sql)){
+          $msg = "Image uploaded and saved in the Database";
+          $msg_class = "alert-success";
+        } else {
+          $msg = "There was an error in the database";
+          $msg_class = "alert-danger";
+        }
+      } else {
+        $error = "There was an erro uploading the file";
+        $msg = "alert-danger";
+      }
+    }
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Image Preview and Upload PHP</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-4 offset-md-4 form-div">
+        <a href="profiles.php">View all profiles</a>
+        <form action="form.php" method="post" enctype="multipart/form-data">
+          <h2 class="text-center mb-3 mt-3">Update profile</h2>
+          <?php if (!empty($msg)): ?>
+            <div class="alert <?php echo $msg_class ?>" role="alert">
+              <?php echo $msg; ?>
+            </div>
+          <?php endif; ?>
+          <div class="form-group text-center" style="position: relative;" >
+            <span class="img-div">
+              <div class="text-center img-placeholder"  onClick="triggerClick()">
+                <h4>Update image</h4>
+              </div>
+              <img src="images/avatar.jpg" onClick="triggerClick()" id="profileDisplay">
+            </span>
+            <input type="file" name="profileImage" onChange="displayImage(this)" id="profileImage" class="form-control" style="display: none;">
+            <label>Profile Image</label>
+          </div>
+          <div class="form-group">
+            <label>Bio</label>
+            <textarea name="bio" class="form-control"></textarea>
+          </div>
+          <div class="form-group">
+            <button type="submit" name="save_profile" class="btn btn-primary btn-block">Save User</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src="script.js"></script>
+</body>
+</html>
+
+
+
+<?php
+  $conn = mysqli_connect("localhost", "root", "", "img-upload");
+  $results = mysqli_query($conn, "SELECT * FROM users");
+  $users = mysqli_fetch_all($results, MYSQLI_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Image Preview and Upload</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-4 offset-md-4" style="margin-top: 30px;">
+        <a href="form.php" class="btn btn-success">New profile</a>
+        <br>
+        <br>
+        <table class="table table-bordered">
+          <thead>
+            <th>Image</th>
+            <th>Bio</th>
+          </thead>
+          <tbody>
+            <?php foreach ($users as $user): ?>
+              <tr>
+                <td> <img src="<?php echo 'images/' . $user['profile_image'] ?>" width="90" height="90" alt=""> </td>
+                <td> <?php echo $user['bio']; ?> </td>
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
